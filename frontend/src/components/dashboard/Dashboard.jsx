6385a38f98fbe26ef2f7fe5dcbc410e2fae9c165
Aryan Manu <aryanmanu544@gmail.com>
2025-03-29 18:47:34 +0530

    added very basic navbar and dashboard

---
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="Dashboard">
+      <h1>Welcome to Grade</h1>
+      <p>Create an account or login to existing account to get started!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
